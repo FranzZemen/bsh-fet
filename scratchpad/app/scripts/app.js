133299a7dee7c5a5ec2cd4a9997fa6a3aec33a74
FranzZemen <FranzZemen@gmail.com>
2016-03-02 05:56:38 -0600

    initial scratchpad

---
+++ scratchpad/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name scratchpadApp
+ * @description
+ * # scratchpadApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('scratchpadApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
